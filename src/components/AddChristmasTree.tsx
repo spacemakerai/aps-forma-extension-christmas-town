44f9959547e90008f9124fbab0fa17833d11dcee
anna-winther-adsk <119871716+anna-winther-adsk@users.noreply.github.com>
2023-11-30 16:39:15 +0100

    More styling

--- src/components/AddChristmasTree.tsx
+++ src/components/AddChristmasTree.tsx
@@ -25,13 +25,24 @@ const getColorArray = (triangleLength: number) => {
 };
 
 export default function AddChristmasTree() {
-  const superClick = async () => {
-    // read glb file
-    const blob = await fetch("./christmas_tree_(11).glb")
-      .then((res) => res.blob())
-      .then((blob) => blob.arrayBuffer());
+  //   const superClick = async () => {
+  //     // read glb file
+  //     const blob = await fetch("./christmas_tree_(11).glb")
+  //       .then((res) => res.blob())
+  //       .then((blob) => blob.arrayBuffer());
 
-    Forma.render.glb.add({ glb: blob });
+  //     Forma.render.glb.add({ glb: blob });
+  //   };
+
+  const superClick = async () => {
+    const treeGeometry = new THREE.ConeGeometry(5, 20, 32);
+    const treeMaterial = new THREE.MeshBasicMaterial({ color: 0x008000, vertexColors: true });
+    const tree = new THREE.Mesh(treeGeometry, treeMaterial);
+    const position = tree.geometry.attributes.position.array as Float32Array;
+    const color = getColorArray(position.length);
+    Forma.render.addMesh({
+      geometryData: { position, color },
+    });
   };
 
   return (
